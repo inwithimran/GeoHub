88d5fd9532a21543f653ed8021e2e0ac54b474b9
Tabib Imran <in.with.imran@gmail.com>
2026-09-03 13:44:04 +0600

    Add files via upload

--- api/[action].js
+++ api/[action].js
@@ -31,6 +31,14 @@ const routes = {
 };
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   const action = req.query.action;
   const fn = routes[action];
   if (!fn) {
