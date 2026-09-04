4d0ed78b6165a55c24ef483f703735864b9bfab9
Tabib Imran <in.with.imran@gmail.com>
2026-09-04 10:25:42 +0600

    Add files via upload

--- resources/android/native-src/MainActivity.kt
+++ resources/android/native-src/MainActivity.kt
@@ -1,1 +1,26 @@
+package com.geohubmmc.app
 
+import android.content.Intent
+import android.os.Bundle
+import com.getcapacitor.BridgeActivity
+
+class MainActivity : BridgeActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleDeepLink(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleDeepLink(intent)
+    }
+
+    private fun handleDeepLink(intent: Intent?) {
+        val url = intent?.getStringExtra("geohub_url") ?: return
+        intent.removeExtra("geohub_url")
+        val escaped = url.replace("\\", "\\\\").replace("\"", "\\\"")
+        bridge?.triggerJSEvent("geohubNotificationTap", "window", "\"$escaped\"")
+    }
+}
