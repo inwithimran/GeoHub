964875aba18fb10e6e1a5e6cf7caeac237b24aa6
Tabib Imran <in.with.imran@gmail.com>
2026-09-04 11:36:54 +0600

    Add files via upload

--- scripts/ci/inject-android-variables.py
+++ scripts/ci/inject-android-variables.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 path = "android/variables.gradle"
@@ -5,16 +6,22 @@ path = "android/variables.gradle"
 with open(path, "r") as f:
     text = f.read()
 
-if "rgcfaIncludeGoogle" in text:
-    sys.exit(0)
+if "rgcfaIncludeGoogle" not in text:
+    marker = "ext {"
+    if marker not in text:
+        print("Could not find 'ext {' block in variables.gradle", file=sys.stderr)
+        sys.exit(1)
+    addition = "ext {\n    rgcfaIncludeGoogle = true\n    androidxCredentialsVersion = '1.3.0'\n"
+    text = text.replace(marker, addition, 1)
 
-marker = "ext {"
-if marker not in text:
-    print("Could not find 'ext {' block in variables.gradle", file=sys.stderr)
+text, count = re.subn(
+    r"minSdkVersion\s*=\s*\d+",
+    "minSdkVersion = 23",
+    text,
+)
+if count == 0:
+    print("Could not find minSdkVersion in variables.gradle", file=sys.stderr)
     sys.exit(1)
-
-addition = "ext {\n    rgcfaIncludeGoogle = true\n    androidxCredentialsVersion = '1.3.0'\n"
-text = text.replace(marker, addition, 1)
 
 with open(path, "w") as f:
     f.write(text)
