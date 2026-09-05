782456883a7bc97e1e48918317334e94eafd7be3
Tabib Imran <in.with.imran@gmail.com>
2026-09-05 19:22:38 +0600

    Splash Update

--- scripts/ci/inject-splash-theme.py
+++ scripts/ci/inject-splash-theme.py
@@ -1,9 +1,31 @@
 import re
+import shutil
 import sys
 
-path = "android/app/src/main/res/values/styles.xml"
+styles_path = "android/app/src/main/res/values/styles.xml"
+asset_source = "resources/android/splash_icon.png"
+asset_dest = "android/app/src/main/res/drawable/splash_icon.png"
+layer_list_dest = "android/app/src/main/res/drawable/launch_background.xml"
 
-with open(path, "r", encoding="utf-8") as f:
+shutil.copyfile(asset_source, asset_dest)
+
+with open(layer_list_dest, "w", encoding="utf-8") as f:
+    f.write(
+        '<?xml version="1.0" encoding="utf-8"?>\n'
+        '<layer-list xmlns:android="http://schemas.android.com/apk/res/android">\n'
+        '    <item android:drawable="#0f2e1d" />\n'
+        '    <item\n'
+        '        android:width="140dp"\n'
+        '        android:height="140dp"\n'
+        '        android:gravity="center">\n'
+        '        <bitmap\n'
+        '            android:src="@drawable/splash_icon"\n'
+        '            android:gravity="fill" />\n'
+        '    </item>\n'
+        '</layer-list>\n'
+    )
+
+with open(styles_path, "r", encoding="utf-8") as f:
     text = f.read()
 
 pattern = re.compile(
@@ -20,17 +42,14 @@ if not match:
     sys.exit(1)
 
 replacement = (
-    '<style name="AppTheme.NoActionBarLaunch" parent="@style/Theme.SplashScreen.IconBackground">'
-    '<item name="windowSplashScreenBackground">#0f2e1d</item>'
-    '<item name="windowSplashScreenAnimatedIcon">@mipmap/ic_launcher_foreground</item>'
-    '<item name="windowSplashScreenIconBackgroundColor">#ffffff</item>'
-    '<item name="postSplashScreenTheme">@style/AppTheme.NoActionBar</item>'
+    '<style name="AppTheme.NoActionBarLaunch" parent="AppTheme.NoActionBar">'
+    '<item name="android:windowBackground">@drawable/launch_background</item>'
     '</style>'
 )
 
 text = text[:match.start()] + replacement + text[match.end():]
 
-with open(path, "w", encoding="utf-8") as f:
+with open(styles_path, "w", encoding="utf-8") as f:
     f.write(text)
 
 print("splash theme patched successfully")
